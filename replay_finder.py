195b39e76968b856bd5997eea21682b3d0ba311f
Matej Straka <strakammm@gmail.com>
2024-11-28 13:02:24 +0100

    feat: Add replay filters

--- replay_finder.py
+++ replay_finder.py
@@ -10,17 +10,32 @@ count = 200  # This is max
 n_workers = 12
 game_type = "1v1"
 
-def filter_by_name(replays, names: list[str]):
-    pass
+
+def filter_by_name(replays, name: str):
+    for replay in replays:
+        player_list = [player["currentName"] for player in replay.get("ranking", [])]
+        if name in player_list:
+            yield replay
+
 
 def filter_by_stars(replays, min_stars: int = 0, max_stars: int = 100):
-    pass
+    for replay in replays:
+        player_list = [player["stars"] for player in replay.get("ranking", [])]
+        if all(min_stars <= stars <= max_stars for stars in player_list):
+            yield replay
 
-def filter_by_type(replays, game_type: str):
-    pass
+
+def filter_by_type(replays, game_types: list[str]):
+    for replay in replays:
+        if replay["type"] in game_types:
+            yield replay
+
 
 def filter_by_turns(replays, min_turns: int = 0, max_turns: int = 1000):
-    pass
+    for replay in replays:
+        if min_turns <= replay["turns"] <= max_turns:
+            yield replay
+
 
 base_url_user = "https://generals.io/api/replaysForUsername?"
 
@@ -36,15 +51,9 @@ def get_replay_batch(username: str, offset: int = 0):
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for HTTP issues
-        data = response.json()
-        counter = 0
-        for replay in data:
-            player_list = replay.get("ranking", [])
-            if len(player_list) != 2:
-                continue
-            if player_list[0]["stars"] > 60 and player_list[1]["stars"] > 60:
-                counter += 1
-        return counter
+        replays = response.json()
+        replays = filter_by_stars(replays, 60, 100)
+        return len(replays)
     except Exception as e:
         print(f"Error fetching offset {offset}: {e}")
         return 0
