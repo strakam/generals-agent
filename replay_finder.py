06f26d743e7ec8bbcafaa74226db55b0bfdf53ef
Matej Straka <strakammm@gmail.com>
2024-11-28 14:28:27 +0100

    feat: Get replay ids

--- replay_finder.py
+++ replay_finder.py
@@ -7,10 +7,7 @@ from functools import partial
 base_url_general = "https://generals.io/api/replays?"
 base_url_user = "https://generals.io/api/replaysForUsername?"
 
-count = 200  # This is max
 n_workers = 12
-game_type = "1v1"
-
 
 def filter_by_name(replays, name: str):
     result = []
@@ -21,7 +18,7 @@ def filter_by_name(replays, name: str):
     return result
 
 
-def filter_by_stars(replays, min_stars: int = 0, max_stars: int = 100):
+def filter_by_elo_game(replays, min_stars: int = 0, max_stars: int = 100):
     result = []
     for replay in replays:
         player_list = [player["stars"] for player in replay.get("ranking", [])]
@@ -29,11 +26,19 @@ def filter_by_stars(replays, min_stars: int = 0, max_stars: int = 100):
             result.append(replay)
     return result
 
-
-def filter_by_type(replays, game_types: list[str]):
+def filter_by_elo_player(replays, min_stars: int = 0, max_stars: int = 100):
     result = []
     for replay in replays:
-        if replay["type"] in game_types:
+        player_list = [player["stars"] for player in replay.get("ranking", [])]
+        if any(min_stars <= stars <= max_stars for stars in player_list):
+            result.append(replay)
+    return result
+
+
+def filter_by_n_players(replays, n_players: int = 2):
+    result = []
+    for replay in replays:
+        if len(replay.get("ranking", [])) == n_players:
             result.append(replay)
     return result
 
@@ -56,33 +61,32 @@ def fetch_replay_batch(url: str, filters: list = []):
         replays = response.json()
         for f in filters:
             replays = f(replays)
-        return len(replays)
+        return replays
     except Exception as e:
         print(f"Error fetching url {url}: {e}")
-        return 0
 
 
 def fetch_replays(username: str = None):
     """
     Fetch replays for a user in parallel using threading.
     """
-    offsets = range(0, 4000, 200)
-    total = 0
+    offsets = range(0, 550_000, 200)
 
     # make partial functions for filters
     filters = [
-        partial(filter_by_type, game_types=[game_type]),
-        partial(filter_by_turns, min_turns=0, max_turns=1e5),
-        # partial(filter_by_stars, min_stars=600, max_stars=1e5),
+        partial(filter_by_n_players, n_players=2),
+        # partial(filter_by_turns, min_turns=0, max_turns=1e5),
+        partial(filter_by_elo_player, min_stars=65, max_stars=1e5),
     ]
 
     if username:
-        url = f"https://generals.io/api/replaysForUsername?u={username}&"
+        url = f"https://generals.io/api/replaysForUsername?&u={username}&"
     else:
         url = "https://generals.io/api/replays?"
 
     urls = [f"{url}offset={offset}&count=200" for offset in offsets]
 
+    replay_ids = []
     # Use ThreadPoolExecutor for concurrent requests
     with ThreadPoolExecutor(max_workers=n_workers) as executor:
         # Submit tasks for all offsets
@@ -93,13 +97,15 @@ def fetch_replays(username: str = None):
         # Use tqdm for a progress bar
         for future in tqdm.tqdm(as_completed(futures), total=len(futures)):
             try:
-                total += future.result()
+                replays = future.result()
+                if replays:
+                    replay_ids.extend([replay["id"] for replay in replays])
             except Exception as e:
                 print(f"Error processing a future: {e}")
 
-    return total
-
+    with open("high_elo_player_replay_ids.txt", "w") as f:
+        f.write("\n".join(replay_ids))
 
 # Run the scraper
 if __name__ == "__main__":
-    print(fetch_replays("Human.exe"))
+    fetch_replays()
