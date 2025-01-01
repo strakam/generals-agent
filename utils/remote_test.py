d8f493ce7ef91682752edbde0361acbd374e6181
Matej Straka <strakammm@gmail.com>
2025-01-01 20:44:11 +0100

    add inits

--- utils/remote_test.py
+++ utils/remote_test.py
@@ -1,8 +1,8 @@
 import argparse
 import torch
 from generals.remote import GeneralsIOClient
-from neuro2 import Neuro2Agent
-from network2 import Network
+from checkpoints.sup106.neuro_agent import NeuroAgent
+from checkpoints.sup106.network import Network
 
 
 parser = argparse.ArgumentParser()
@@ -10,14 +10,14 @@ parser.add_argument("--lobby", default=None, type=str, help="Replay ID")
 
 
 def main(args):
-    network = torch.load("checkpoints2/epoch=0-step=78000.ckpt", map_location="cpu")
+    network = torch.load("checkpoints/sup106/epoch=0-step=78000.ckpt", map_location="cpu")
     state_dict = network["state_dict"]
     model = Network(lr=1e-4, n_steps=9, input_dims=(29, 24, 24), compile=True)
     model_keys = model.state_dict().keys()
     filtered_state_dict = {k: v for k, v in state_dict.items() if k in model_keys}
     model.load_state_dict(filtered_state_dict)
 
-    agent = Neuro2Agent(model)
+    agent = NeuroAgent(model)
     user_id = "[Bot]testerboi"
     lobby_id = args.lobby
     with GeneralsIOClient(agent, user_id) as client:
